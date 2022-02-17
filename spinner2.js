bf3bcb3557f912b71297d17759662f82c4879fb1
Manuel Casanova <manucasanova@hotmail.com>
2022-02-16 16:39:27 -0800

    first commit function works but not in the same place

---
+++ spinner2.js
@@ -0,0 +1,16 @@
+let spinner = "| / - \\ | / - \\ |"
+let delay = 0
+for (let i = 0; i < spinner.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(spinner[i])
+  }, delay)
+  delay += 50
+}
+
+
+
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
